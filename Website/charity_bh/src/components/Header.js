e8b975b5acbbaac69d796955344c61c18959bed9
Prajwal Rao <prajwal.ar01@gmail.com>
2021-04-18 03:53:04 +0530

    Website structured

---
+++ Website/charity_bh/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'reactstrap'
+
+
+function Header() {
+    return (
+        <div>
+            <Button />
+            <h1>Header on top</h1>
+        </div>
+    )
+}
+
+export default Header
