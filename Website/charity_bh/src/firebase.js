cc7b411b75f1a12eac51822385c4487f8cf56f3a
Prajwal Rao <prajwal.ar01@gmail.com>
2021-04-18 13:24:54 +0530

    firebase init

---
+++ Website/charity_bh/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCAEDC0IPBG6wq2OXUHxR6gnDh0SvXDlHI",
+    authDomain: "charitybh-dc293.firebaseapp.com",
+    projectId: "charitybh-dc293",
+    storageBucket: "charitybh-dc293.appspot.com",
+    messagingSenderId: "620921666149",
+    appId: "1:620921666149:web:8546090aad1c06890ae48c",
+    measurementId: "G-ZVCBNERB1T"
+  };
